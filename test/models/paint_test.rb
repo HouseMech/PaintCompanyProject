56f7c62c7def16df4f657df57b8624cffd3ac6f8
HouseMech <liam.harbec@gmail.com>
2024-03-22 16:03:48 -0700

    Added validations for Paint

--- test/models/paint_test.rb
+++ test/models/paint_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PaintTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validations' do
+    def it_has_a_colour
+      paint = Paint.new([:colour] => 'black')
+      assert paint.colour == 'black'
+    end
+
+    def it_has_a_stock
+      paint = Paint.new([:stock] => 7)
+      assert paint.stock == 7
+    end
+  end
 end
