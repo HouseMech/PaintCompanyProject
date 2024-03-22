50a214b63b9637503e9ac02ea2b3c1162fe8940b
HouseMech <liam.harbec@gmail.com>
2024-03-22 16:52:46 -0700

    Added status to Paint

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_231117) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_22_233623) do
   create_table "paints", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
     t.integer "stock"
+    t.string "status"
   end
 
 end
