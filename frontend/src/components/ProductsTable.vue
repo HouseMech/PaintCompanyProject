e595088a430466ab743f42d101d8864064ee61f3
HouseMech <liam.harbec@gmail.com>
2024-03-23 21:17:37 -0700

    Added a form error to the ProductsTable

--- frontend/src/components/ProductsTable.vue
+++ frontend/src/components/ProductsTable.vue
@@ -8,40 +8,44 @@
         <v-dialog v-model="dialog" max-width="500px">
           <v-card>
             <v-card-title>
-              <span class="text-h5">Edit</span>
+              <span class="text-h5">Edit - {{ editedItem.colour }}</span>
             </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" md="6" sm="6">
-                    <v-select v-model="editedItem.status" :items="items" label="Select Status">
-                      <template v-slot:selection="{ item }">
-                        <StatusChip :status="item.title" />
-                      </template>
-                      <template v-slot:item="{ props, item }">
-                        <v-list-item v-bind="props" title="">
+            <v-form validate-on="submit lazy" @submit.prevent="save">
+              <v-card-text>
+                <v-container>
+                  <v-row v-if="formError">
+                    <v-alert color="error" icon="$error" :text="formError"></v-alert>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="12" md="6" sm="6">
+                      <v-select v-model="editedItem.status" :items="items" label="Select Status">
+                        <template v-slot:selection="{ item }">
                           <StatusChip :status="item.title" />
-                        </v-list-item>
-                      </template>
-                    </v-select>
-                  </v-col>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-text-field v-model="editedItem.stock" label="Stock"></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
+                        </template>
+                        <template v-slot:item="{ props, item }">
+                          <v-list-item v-bind="props" title="">
+                            <StatusChip :status="item.title" />
+                          </v-list-item>
+                        </template>
+                      </v-select>
+                    </v-col>
+                    <v-col cols="12" md="4" sm="6">
+                      <v-text-field v-model="editedItem.stock" label="Stock"></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="close">
-                Cancel
-              </v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="save">
-                Save
-              </v-btn>
-            </v-card-actions>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue-darken-1" variant="text" type="submit">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-form>
           </v-card>
         </v-dialog>
       </v-toolbar>
@@ -83,6 +87,7 @@ export default {
         { title: 'Actions', key: 'actions', sortable: false }
       ],
       items: ['out of stock', 'running low', 'available'],
+      formError: ''
     }
   },
   methods: {
@@ -108,6 +113,7 @@ export default {
     },
     close() {
       this.dialog = false
+      this.formError = ''
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
@@ -117,10 +123,11 @@ export default {
       this.$store.dispatch('paints/updatePaint', { id: this.editedIndex, status: this.editedItem.status, stock: this.editedItem.stock, }).then((result) => {
         if (result.status === 200) {
           //close the modal, update the dashboard.
+          this.close()
         }
       }).catch((error) => {
+        this.formError = "Something went wrong"
         console.log(error)
-
       })
     },
   },
