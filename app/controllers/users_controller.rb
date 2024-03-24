5255055e813efb825651ef81cf17acc3d65425b5
HouseMech <liam.harbec@gmail.com>
2024-03-24 11:52:40 -0700

    Began working on admin functionality and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_admin, only: [:index]
+  before_action :check_admin, only: [:index, :update]
 
   def index
     @users = User.all
     render json: @users
+  end
+
+  def update
+    if @user.update(paint_params)
+
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -16,6 +25,14 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:role, :is_active)
+  end
+
   def user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.devise[:jwt_secret_key]).first
     user_id = jwt_payload['sub']
---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    HELLO MY FRIEND
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped></style>
