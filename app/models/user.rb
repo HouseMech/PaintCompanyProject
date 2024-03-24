e56e08c525d5022bf48d17baa5a845c200a3e9be
HouseMech <liam.harbec@gmail.com>
2024-03-24 08:34:55 -0700

    changed base role from user to default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  enum role: {user: 0, painter: 1, admin: 2}
+  enum role: {default: 0, painter: 1, admin: 2}
   # Users can view the paints and their statuses, but not make any changes
   # Painters can modify the paint stock, as they consume it during work
   # Managers can
